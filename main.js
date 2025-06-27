b4ac6bcd87166d7d842bda66b79d5cb1b74fc935
crissouza15 <160858254+crissouza15@users.noreply.github.com>
2025-06-27 08:36:37 -0300

    ..

--- main.js
+++ main.js
@@ -77,7 +77,6 @@ forcaSenha.classList.add('forte');
 else if (tamanhoSenha > 5 && tamanhoSenha < 12 ){
 forcaSenha.classList.add('media');
 }else if (tamanhoSenha <= 5){
-
-forcaSenha.classList.add('forte');
+forcaSenha.classList.add('fraca');
 }
 }
