011b53b6bb8402c54867bf2a5ac50ed67dbdaf8b
crissouza15 <160858254+crissouza15@users.noreply.github.com>
2025-06-27 07:49:08 -0300

    ..

--- main.js
+++ main.js
@@ -1,9 +1,13 @@
 const numeroSenha = document.querySelector('.parametro-senha__texto');
 let tamanhoSenha = 12;
 numeroSenha.textContent = tamanhoSenha;
-
+const letrasMaiusculas = 'ABCDEFGHIJKLMNOPQRSTUVXYWZ';
+const letrasMinusculas = 'abcdefghijklmnopqrstuvxywz';
+const numeros = '0123456789';
+const simbolos = '!@%*?';
 const botoes = document.querySelectorAll('.parametro-senha__botao');
-
+const campoSenha = document.querySelector('#campo-senha');
+const checkbox = document.querySelectorAll('.checkbox');
 botoes[0].onclick = diminuiTamanho;
 botoes[1].onclick = aumentaTamanho;
 
@@ -62,3 +66,7 @@ function geraSenha() {
     }
     campoSenha.value = senha;
 }   
+function classificaSenha(){
+forcaSenha.classList.remove('fraca','media','forte');
+forcaSenha.classList.add('forte');
+}
